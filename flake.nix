f78d9982fcf4d79502f87bb403f42e4e97bbd143
Valdemar Grange <randomvald0069@gmail.com>
2023-10-13 03:02:41 +0200

    work

--- flake.nix
+++ flake.nix
@@ -4,4 +4,16 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
+
+  outputs = { self, nixpkgs }: {
+    nixpkgs.services.xserver = {
+      enabled = true;
+      displayManager = {
+        defaultSession = "none+i3";
+      };
+      windowMaanger.i3 = {
+        enable = true;
+      };
+    };
+  };
 }
