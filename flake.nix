360912a437a930856405a5cb27ed6efe859d9c02
ValdemarGr <randomvald0069@gmail.com>
2023-10-13 23:04:01 +0200

    work on nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,30 @@
   
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixpkgs.services.xserver = {
-      enabled = true;
-      displayManager = {
-        defaultSession = "none+i3";
+  outputs = { self, nixpkgs, flake-utils }: 
+    let 
+      system = flake-utils.lib.system.x86_64-linux;
+      machine = "valde";
+    in {
+      nixosConfigurations.${machine} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ({ pkgs, ...}: {
+          })
+        ];
       };
-      windowMaanger.i3 = {
-        enable = true;
-      };
+      # use mkShell
+      devShells.${system}.${machine} = nixpkgs.legacyPackages.${system}.pkgs.mkShell {
+        name = "hoy";
+        buildInputs = [
+          nixpkgs.legacyPackages.${system}.pkgs.curl
+        ];
+        shellHook = ''
+          echo "Ohoy!"
+        '';
     };
   };
 }
