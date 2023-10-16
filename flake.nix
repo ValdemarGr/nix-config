2edbe6e6aa129e9e55cebe40172fc116b1471da4
Valdemar Grange <randomvald0069@gmail.com>
2023-10-16 01:25:43 +0000

    work

--- flake.nix
+++ flake.nix
@@ -2,11 +2,15 @@
   description = "Valde's nixos configuration";
   
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+    	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = { self, nixpkgs, flake-utils, home-manager }: 
     let 
       system = flake-utils.lib.system.x86_64-linux;
       machine = "valde";
@@ -15,7 +19,12 @@
 	inherit system;
 	modules = [
 		(nixpkgs + "/nixos/modules/installer/virtualbox-demo.nix")
+		home-manager.nixosModules.home-manager
 		({pkgs, ...}: {
+			users.users.${machine} = {
+				isNormalUser = true;
+				extraGroup = [ "wheel" ];
+			};
  			environment.systemPackages = [pkgs.gh];
 			programs.git.enable = true;
 			programs.neovim.enable = true;
