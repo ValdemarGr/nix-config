4d2826d45ffb4b005b0b4eebde0f854238bea267
Valdemar Grange <randomvald0069@gmail.com>
2024-07-05 14:31:07 +0200

    work

--- system/machine/work/hardware-configuration.nix
+++ system/machine/work/hardware-configuration.nix
@@ -40,10 +40,10 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    # driSupport = true;
+    # driSupport32Bit = true;
   };
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
