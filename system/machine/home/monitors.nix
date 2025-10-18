7eba04b7bc41b09dfd87b6b7e0475ee129f69429
Valdemar Grange <randomvald0069@gmail.com>
2025-10-18 15:26:02 +0200

    mon

--- system/machine/home/monitors.nix
+++ system/machine/home/monitors.nix
@@ -8,16 +8,6 @@
         position = 0x0
         scale = 1
         bitdepth = 10
-        cm = hdr
-        sdrbrightness = 1.1
-        sdrsaturation = 1.1
-        supports_wide_color = true
-        supports_hdr = true
-        sdr_min_luminance = 0.03
-        sdr_max_luminance = 200
-        min_luminance = 0
-        max_luminance = 1000
-        max_avg_luminance = 200
       }
     	monitor = DP-1,1920x1080@144,3440x0,1
         	'';
