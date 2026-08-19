63a64f396c54fa98a78ab3b111d09e688636ca50
Valdemar Grange <randomvald0069@gmail.com>
2026-08-19 19:34:27 +0200

    fix monitors

--- system/machine/work/hardware-configuration.nix
+++ system/machine/work/hardware-configuration.nix
@@ -28,4 +28,10 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.graphics = {
+    enable = true;
+    # driSupport = true;
+    # driSupport32Bit = true;
+  };
 }
--- system/machine/work/monitors.nix
+++ system/machine/work/monitors.nix
@@ -1,21 +1,21 @@
 {
   primary = "DP-1";
-  other = [ "DP-2" "HDMI-A-2" ];
+  other = [ "DP-2" "DP-3" ];
   monitor-config = ''
     hl.monitor({
-      output = "DP-1",
+      output = "DP-2",
       mode = "3440x1440@59.99900",
       position = "2560x0",
       scale = 1,
     })
     hl.monitor({
-      output = "DP-2",
+      output = "DP-1",
       mode = "2560x1440@59.99900",
       position = "0x0",
       scale = 1,
     })
     hl.monitor({
-      output = "HDMI-A-2",
+      output = "DP-3",
       mode = "2560x1440@59.99900",
       position = "6000x0",
       scale = 1,
